d803aa3266a078e95a597fdaacd1ff400fa3fad2
Rambabu malgaya <80635877+Rambabu-malgaya@users.noreply.github.com>
2023-04-24 15:19:22 +0530

    Update 3Sum.cpp

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,12 +1,4 @@
-/*
-    Given int array, return all unique triplets that sum to 0
-    Ex. nums = [-1,0,1,2,-1,-4] -> [[-1,-1,2],[-1,0,1]]
 
-    Sort, for each i, have j & k go outside in, check for 0 sums
-
-    Time: O(n^2)
-    Space: O(n)
-*/
 
 class Solution {
 public:
