ff1533f1753b8e5e6a769320e418295e28cba07b
Rambabu malgaya <80635877+Rambabu-malgaya@users.noreply.github.com>
2023-04-26 22:01:17 +0530

    Update raun watter.cpp

--- raun watter.cpp
+++ raun watter.cpp
@@ -1,14 +1,3 @@
-/*
-    Given elevation map array, compute trapped water
-    Ex. height = [0,1,0,2,1,0,1,3,2,1,2,1] -> 6
-
-    2 pointers, outside in, track max left/right
-    For lower max, curr only dependent on that one
-    Compute height of these, iterate lower one
-
-    Time: O(n)
-    Space: O(1)
-*/
 
 class Solution {
 public:
