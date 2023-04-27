cb74be739455433c43b302eebf65f215fd48a0f3
Rambabu malgaya <80635877+Rambabu-malgaya@users.noreply.github.com>
2023-04-27 18:59:16 +0530

    Add files via upload

---
+++ valid parenthisis.cpp
@@ -0,0 +1,31 @@
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> open;
+        unordered_map<char, char> parens = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'},
+        };
+        
+        for (const auto& c : s) {
+            if (parens.find(c) != parens.end()) {
+                // if input starts with a closing bracket.
+                if (open.empty()) {
+                    return false;
+                }
+
+                if (open.top() != parens[c]) {
+                    return false;
+                }
+
+                open.pop();
+            } else {
+                open.push(c);
+            }
+        }
+        
+        return open.empty();
+    }
+};
