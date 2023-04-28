44bb8a5ce93a1de68ad2046d55db727e14fc6822
Rambabu malgaya <80635877+Rambabu-malgaya@users.noreply.github.com>
2023-04-28 20:12:31 +0530

    Add files via upload

---
+++ minstack.cpp
@@ -0,0 +1,40 @@
+
+
+class MinStack {
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        stk.push(val);
+        
+        if (minStk.empty() || val < minStk.top().first) {
+            minStk.push({val, 1});
+        } else if (val == minStk.top().first) {
+            minStk.top().second++;
+        }
+    }
+    
+    void pop() {
+        if (stk.top() == minStk.top().first) {
+            minStk.top().second--;
+            if (minStk.top().second == 0) {
+                minStk.pop();
+            }
+        }
+        stk.pop();
+    }
+    
+    int top() {
+        return stk.top();
+    }
+    
+    int getMin() {
+        return minStk.top().first;
+    }
+private:
+    stack<int> stk;
+    stack<pair<int, int>> minStk;
+};
+
