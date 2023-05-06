7a37e2c2f917f97dbaa183e517a4d38a46df533c
Rambabu malgaya <80635877+Rambabu-malgaya@users.noreply.github.com>
2023-05-06 12:25:20 +0530

    Add files via upload

---
+++ find minimumin sorted array.cpp
@@ -0,0 +1,34 @@
+
+class Solution
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+        
+        int res = nums[0];
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l <= r)
+        {
+            if (nums[l] < nums[r])
+            {
+                res = min(res, nums[l]);
+                break;
+            }
+            int mid = l + (r - l) / 2;
+            res = min(res, nums[mid]);
+
+            if (nums[mid] >= nums[l]) // mid present in left sorted array
+            {
+                l = mid +1 ; 
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+
+        return res;
+    }
+};
