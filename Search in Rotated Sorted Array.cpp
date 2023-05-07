8cb14289741f567cc5e2237ae8c1e94578833999
Rambabu malgaya <80635877+Rambabu-malgaya@users.noreply.github.com>
2023-05-07 21:02:50 +0530

    Update Search in Rotated Sorted Array.cpp

--- Search in Rotated Sorted Array.cpp
+++ Search in Rotated Sorted Array.cpp
@@ -3,8 +3,7 @@ class Solution {
   int search(vector<int>& nums, int target) {
     int l = 0;
     int r = nums.size() - 1;
-
-    while (l <= r) {
+while (l <= r) {
       const int m = (l + r) / 2;
       if (nums[m] == target)
         return m;
