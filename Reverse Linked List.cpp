65688405cbf5dd31f6a49ece57c9c2a6a8911724
Rambabu malgaya <80635877+Rambabu-malgaya@users.noreply.github.com>
2023-05-16 22:44:32 +0530

    Create Reverse Linked List.cpp

---
+++ Reverse Linked List.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        ListNode *prev = NULL;
+        ListNode *curr = head;
+
+        while (curr != NULL)
+        {
+            ListNode *temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+};
