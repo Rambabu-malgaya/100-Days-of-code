4173ed8f9a90b36a7fa51812759e743e1564b37d
Rambabu-malgaya <rambabumalgaya7@gmail.com>
2023-06-08 12:40:47 +0530

    Updating repo

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#c++ code for adding two numbers
+
+
+write a program for add two no.
+
+#include<iostream>
+using namespace std;
+int main()
+{                   
+
+
+
+
+
+
+int a,b,c;
+
+cout<<"enter the value of a and b"; 
+cin>>a>>b;
+
+
+c=a+b;
