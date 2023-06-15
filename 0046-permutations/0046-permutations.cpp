c2ea74c3ac42c56530709732332c7abd66b1239d
Rambabu malgaya <80635877+Rambabu-malgaya@users.noreply.github.com>
2023-06-15 23:29:05 +0530

    Time: 3 ms (68.64%), Space: 7.7 MB (64.61%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        dfs(nums, 0, result);
+        return result;
+    }
+private:
+    void dfs(vector<int>& nums, int start, vector<vector<int>>& result) {
+        if (start == nums.size()) {
+            result.push_back(nums);
+            return;
+        }
+        for (int i = start; i < nums.size(); i++) {
+            swap(nums[i], nums[start]);
+            dfs(nums, start + 1, result);
+            swap(nums[i], nums[start]);
+        }
+    }
+};
