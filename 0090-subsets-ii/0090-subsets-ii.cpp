087ef5e2c979a43bf745d69cd0b409d882ee90fe
Rambabu malgaya <80635877+Rambabu-malgaya@users.noreply.github.com>
2023-06-17 00:00:45 +0530

    Time: 0 ms (100%), Space: 7.3 MB (92.93%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        vector<int> curr;
+        vector<vector<int>> result;
+        
+        dfs(nums, 0, curr, result);
+        return result;
+    }
+private:
+    void dfs(vector<int>& nums, int start, vector<int>& curr, vector<vector<int>>& result) {
+        result.push_back(curr);
+        for (int i = start; i < nums.size(); i++) {
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            curr.push_back(nums[i]);
+            dfs(nums, i + 1, curr, result);
+            curr.pop_back();
+        }
+    }
+};
